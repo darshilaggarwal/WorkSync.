b9bc5aa7ece74bb2f0c1d2a20596ec333a1f12af
Darshil Aggarwal <darshil@Darshils-MacBook-Air.local>
2025-01-12 21:12:56 +0530

    Login functionality

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,46 @@
 import React from 'react'
+import { useState } from 'react';
+
+
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+
+    const SubmitHandler = (e)=>{
+        e.preventDefault()
+        console.log("email is", email);
+        console.log("password is", password);
+    
+        setEmail("")
+        setPassword("")
+    
+        
+    }
+
   return (
     <div>
       <div className='bg-zinc-900 w-screen h-screen flex justify-center items-center'> 
     <div className='border-2 border-emerald-500 p-20 rounded-lg'> 
-        <form className='flex flex-col items-center justify-center gap-8' >
-            <input className='text-gray-400 outline-none border-2 border-emerald-500 bg-transparent rounded-[50px] p-4'
+        <form onSubmit={(e)=>{
+            SubmitHandler(e)
+        }}
+        className='flex flex-col items-center justify-center gap-6' >
+
+            <input value={email} onChange={(e)=>{
+                    setEmail(e.target.value)
+              }}
+            className='text-gray-50 outline-none border-2 border-emerald-500 bg-transparent rounded-[50px] p-4'
              type="email" placeholder='Enter your e-mail' required/>
-            <input className='text-gray-400 outline-none border-2 border-emerald-500 bg-transparent rounded-[50px] p-4'
+
+            <input value={password} onChange={(e)=>{
+                    setPassword(e.target.value)
+              }}
+            className='text-gray-50 outline-none border-2 border-emerald-500 bg-transparent rounded-[50px] p-4'
              type="password" placeholder='Enter your password' required />
+
             <button className='p-3 bg-emerald-500 rounded-[50px]'
             >Submit</button>
         </form>
