516f8706bbcb4bd5e2ca4823fb4b776e0370df8e
Darshil Aggarwal <darshil@Darshils-MacBook-Air.local>
2025-01-12 14:12:48 +0530

    initialised project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-slate-600 bg-slate-800'
+      >hello</h1>
+    </div>
+  )
+}
+
+export default App
